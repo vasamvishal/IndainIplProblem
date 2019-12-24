50473283db0b9e3eee58c665c8822799503987ab
vasamvishal12@gmail.com <vasamvishal12@gmail.com>
2019-12-24 15:17:27 +0530

    ADD:Testcase for reding iplbowler file using mockito

--- src/test/java/iplpackage/MockitoClass.java
+++ src/test/java/iplpackage/MockitoClass.java
@@ -16,33 +16,46 @@ import static org.mockito.Mockito.when;
 
 public class MockitoClass {
     private static String IPL_BATSMAN_DATA = "/home/user/new ipl folder/src/test/resources/IPL2019FactsheetMostRuns.csv";
-    Map<String, IPLDAO> map=null;
+    private static String BOWLER_IPL_LOAD_DATA = "/home/user/new ipl folder/src/test/resources/IPL2019FactsheetMostWkts.csv";
+
+    Map<String, IPLDAO> map = new HashMap<>();
     @Mock
     IPLAdapter adapter;
 
     @Rule
     public MockitoRule mockitoRule= MockitoJUnit.rule();
 
-
+    public void name() {
+        this.map.put("DAvid Warner", new IPLDAO("1", "David Warner ", "12", "12",
+                "2", "692", "100*", "69.2", "481", "143.86",
+                "1", "8", "57", "21"));
+        this.map.put("Virat kohli", new IPLDAO("1", "David Warner ", "12", "12",
+                "2", "692", "100*", "69.2", "481", "143.86",
+                "1", "8", "57", "21"));
+        this.map.put("Ms Dhoni", new IPLDAO("1", "David Warner ", "12", "12",
+                "2", "692", "100*", "69.2", "481", "143.86",
+                "1", "8", "57", "21"));
+    }
 
     @Test
     public void givenIPLBattingFile_ShouldReturnSizeOf3UsingMockito() throws IPLBatsmenException {
-            Map<String, IPLDAO> map = new HashMap<>();
-            map.put("Vishal", new IPLDAO("1", "David Warner ", "12", "12",
-                    "2", "692", "100*", "69.2", "481", "143.86",
-                    "1", "8", "57", "21"));
-            map.put("Vishal12", new IPLDAO("1", "David Warner ", "12", "12",
-                    "2", "692", "100*", "69.2", "481", "143.86",
-                    "1", "8", "57", "21"));
-            map.put("Vishal123", new IPLDAO("1", "David Warner ", "12", "12",
-                    "2", "692", "100*", "69.2", "481", "143.86",
-                    "1", "8", "57", "21"));
-
-        BatsmanAdaptor batsmanAdaptor= mock(BatsmanAdaptor.class);
+        name();
+        BatsmanAdaptor BatsmanAdapter= mock(BatsmanAdaptor.class);
         IplAnalyser iplAnalyser1 = new IplAnalyser();
-        iplAnalyser1.setValue(batsmanAdaptor);
-        when(batsmanAdaptor.loadIplData(IPL_BATSMAN_DATA)).thenReturn((map));
+        iplAnalyser1.setValue(BatsmanAdapter);
+        when(BatsmanAdapter.loadIplData(IPL_BATSMAN_DATA)).thenReturn((this.map));
         Map<String, IPLDAO> map1 = iplAnalyser1.loadIplData(SortingTypes.BATSMAN, IPL_BATSMAN_DATA);
+        Assert.assertEquals(3,map1.size());
+    }
+
+    @Test
+    public void givenIPLBowlingFile_ShouldReturnSizeof3UsingMockito() throws IPLBatsmenException {
+        name();
+        BowlerAdaptor BowlerAdaptor=mock(BowlerAdaptor.class);
+        IplAnalyser iplAnalyser1 = new IplAnalyser();
+        iplAnalyser1.setValue(BowlerAdaptor);
+        when(BowlerAdaptor.loadIplData(BOWLER_IPL_LOAD_DATA)).thenReturn((this.map));
+        Map<String, IPLDAO> map1 = iplAnalyser1.loadIplData(SortingTypes.BOWLER, BOWLER_IPL_LOAD_DATA);
         Assert.assertEquals(3,map1.size());
     }
 }
