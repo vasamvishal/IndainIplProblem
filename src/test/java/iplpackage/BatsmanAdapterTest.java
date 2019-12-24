841aa1ada919865f0e50720609a5c5ab9464ef30
shivanjalisangale30 <shivanjalisangale5456@gmail.com>
2019-12-24 19:10:47 +0530

    Added:Test case for batting Adapter for returning records of both bowling and batting

--- src/test/java/iplpackage/BatsmanAdapterTest.java
+++ src/test/java/iplpackage/BatsmanAdapterTest.java
@@ -7,11 +7,26 @@ import java.util.Map;
 
 public class BatsmanAdapterTest {
     private static String IPL_BATSMAN_DATA = "/home/admin1/IPLVishal/IndainIplProblem/src/test/resources/IPL2019FactsheetMostRuns.csv";
+    private static String BOWLER_IPL_LOAD_DATA = "/home/admin1/IPLVishal/IndainIplProblem/src/test/resources/IPL2019FactsheetMostWkts.csv";
+
     @Test
     public void givenClassShouldLoadFile_Returnrecords() {
         try {
             BatsmanAdaptor batsmanAdaptor = new BatsmanAdaptor();
             Map<String, IPLDAO> stringIPLDAOMap = batsmanAdaptor.loadIplData(IPLBatsmenCSV.class, IPL_BATSMAN_DATA);
+            Assert.assertEquals(100,stringIPLDAOMap.size());
+        }
+        catch (Exception e)
+        {
+
+        }
+    }
+    @Test
+    public void givenClassShouldLoadFileforBattingandbowling_AndReturnAverageOfBatsman() {
+        try {
+            BatsmanAdaptor batsmanAdaptor = new BatsmanAdaptor();
+            Map<String, IPLDAO> stringIPLDAOMap = batsmanAdaptor.loadIplData(IPLBatsmenCSV.class, IPL_BATSMAN_DATA);
+            batsmanAdaptor.loadBatsmanMap(stringIPLDAOMap,BOWLER_IPL_LOAD_DATA);
             Assert.assertEquals(100,stringIPLDAOMap.size());
         }
         catch (Exception e)
